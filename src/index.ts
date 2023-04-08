38f5c9640bbcece48ff6a23af3b9f6db05b7cebb
Nonpavit Detbun <non.devtech@gmail.com>
2023-04-09 04:59:45 +0700

    add group and guard & disable title endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,35 @@
-import { Elysia } from 'elysia'
+import { Elysia, t } from 'elysia'
 
 const app = new Elysia()
-    .get('/search', ({ query }) => `query: ${query.q}`)
-    .get('/search/movie', ({ query }) => `query: ${query.q}`)
-    .get('/search/tv', ({ query }) => `query: ${query.q}`)
-    .get('/search/person', ({ query }) => `query: ${query.q}`)
-    .get('/search/company', ({ query }) => `query: ${query.q}`)
-    .get('/search/episode', ({ query }) => `query: ${query.q}`)
-    .get('/search/review', ({ query }) => `query: ${query.q}`)
-    .get('/search/award', ({ query }) => `query: ${query.q}`)
-    .get('/title/:id', ({ params }) => `id: ${params.id}`)
-    .get('/title/:id/episodes', ({ params }) => `id: ${params.id}`)
-    .get('/title/:id/cast', ({ params }) => `id: ${params.id}`)
-    .get('/title/:id/reviews', ({ params }) => `id: ${params.id}`)
-    .get('/title/:id/awards', ({ params }) => `id: ${params.id}`)
+    .group('/search', (app) => {
+        return app.guard(
+            {
+              schema: {
+                query: t.Object({
+                  q: t.String(),
+                })
+              }
+            },
+            (app) => app
+        .get('/', ({ query }) => `query: ${query.q}`)
+        .get('/movie', ({ query }) => `query: ${query.q}`)
+        .get('/tv', ({ query }) => `query: ${query.q}`)
+        .get('/person', ({ query }) => `query: ${query.q}`)
+        .get('/company', ({ query }) => `query: ${query.q}`)
+        .get('/episode', ({ query }) => `query: ${query.q}`)
+        .get('/review', ({ query }) => `query: ${query.q}`)
+        .get('/award', ({ query }) => `query: ${query.q}`)
+        )
+    })
+
+    // .group('/title/:id', (app) => {
+    //     return app
+    //     .get('/', ({ params }) => `id: ${params.id}`)
+    //     .get('/episodes', ({ params }) => `id: ${params.id}`)
+    //     .get('/cast', ({ params }) => `id: ${params.id}`)
+    //     .get('/reviews', ({ params }) => `id: ${params.id}`)
+    //     .get('/awards', ({ params }) => `id: ${params.id}`)
+    // })
     .listen(3000)
 
 console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`)
